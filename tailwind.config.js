840bab7a82a4e4b3f1b8e69fcb46d55d01c98d0b
Radio Tatuape <radio@tatuapefm.com.br>
2025-06-04 18:28:34 -0300

    config: update Tailwind and PostCSS configuration for better performance

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html'
   ],
   darkMode: 'class',
   theme: {
@@ -22,4 +24,3 @@ module.exports = {
   },
   plugins: [],
 }
-
